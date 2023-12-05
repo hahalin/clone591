a6b5d5246d518911ee60238857893dd10b3f559c
Frank Lin <frank.lin@acer.com>
2023-12-05 17:54:51 +0800

    add regionSelector and bootstrap 5

--- src/App.js
+++ src/App.js
@@ -1,19 +1,42 @@
 import React, { useState } from 'react';
-import Header from "./components/Header"
+import Header from "./components/Header";
 import RegionSelector from "./components/RegionSelector";
 
 function App() {
   const [regions, setRegions] = useState([
-    { name: "台北市", active: true },
+    { name: "台北市", active: false },
     { name: "新北市", active: true },
-    // ...其他地區
+    { name: "基隆市", active: false },
+    { name: "桃園市", active: false }
   ]);
+  const [selectedRegion, setSelectedRegion] = useState(null);
+
+  const handleRegionSelect = (region) => {
+    setSelectedRegion(region);
+    // 更新 regions，將選中的地區設為 active
+    setRegions(regions.map(r => ({
+      ...r,
+      active: r.name === region.name
+    })));
+  };
 
   return (
-    <div>
-      <Header></Header>
-      <RegionSelector regions={regions} />
-      
+    <div className="container">
+      <div className="row">
+        <div className="col-md-12">
+          <Header />
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-md-12">
+          <div className="col-md-4">
+            <RegionSelector regions={regions} onRegionSelect={handleRegionSelect} />
+          </div>
+          <div className="col-md-8">
+            {selectedRegion && <div>選擇的地區: {selectedRegion.name}</div>}
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
--- src/components/RegionSelector.js
+++ src/components/RegionSelector.js
@@ -1,15 +1,26 @@
 import React, { useState } from 'react';
 
-const RegionSelector = ({ regions }) => {
+const RegionSelector = ({ regions,onRegionSelect  }) => {
+
+    const [selectedRegion, setSelectedRegion] = useState(null);
+
+    const handleSelect = (region) => {
+        setSelectedRegion(region);
+        if(onRegionSelect) {
+            onRegionSelect(region);
+        }
+    };
+
+
     return (
         <div className="dropdown">
             <button className="btn btn-secondary dropdown-toggle" type="button" id="regionDropdown" data-bs-toggle="dropdown" aria-expanded="false">
-                選擇地區
+            {selectedRegion ? selectedRegion.name : '選擇地區'}
             </button>
             <ul className="dropdown-menu" aria-labelledby="regionDropdown">
                 {regions.map((region, index) => (
-                    <li key={index}>
-                        <a className={`dropdown-item ${region.active ? 'active' : ''}`} href="#">
+                    <li key={index} onClick={() => handleSelect(region)}>
+                        <a className={`dropdown-item ${selectedRegion && region.name === selectedRegion.name ? 'active' : ''}`} href="#">
                             {region.name}
                         </a>
                     </li>
