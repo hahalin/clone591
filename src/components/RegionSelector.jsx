be2d0d3e02454236fcf0743b2ad17cac1c249329
franklin <hahalin@gmail.com>
2024-01-07 20:51:57 +0800

    use react-bootstrap Dropdown

--- src/components/RegionSelector.jsx
+++ src/components/RegionSelector.jsx
@@ -1,29 +1,30 @@
-import React, { useState,useContext } from 'react';
-import {RegionContext} from "../context/RegionContext";
+import React, { useState, useContext } from 'react';
+import Dropdown from 'react-bootstrap/Dropdown';
+import { RegionContext } from "../context/RegionContext";
 
 
 const RegionSelector = () => {
 
     const { regions, selectedRegion, handleRegionSelect } = useContext(RegionContext);
-
     return (
         <>
-        <div className="dropdown nav-link px-2">
-            <button className="btn btn-secondary dropdown-toggle" type="button" id="regionDropdown" data-bs-toggle="dropdown" aria-expanded="false">
-            {selectedRegion ? selectedRegion.name : '選擇地區'}
-            </button>
-            <ul className="dropdown-menu" aria-labelledby="regionDropdown">
-                {regions.map((region, index) => (
-                    <li key={region.id} onClick={() => handleRegionSelect(region)}>
-                        <a className={`dropdown-item ${selectedRegion && region.name === selectedRegion.name ? 'active' : ''}`} href="#">
-                            {region.name}
-                        </a>
-                    </li>
-                ))}
-            </ul>
-            
-        </div>
-        
+            <div className="dropdown nav-link px-2">
+                <Dropdown>
+                    <Dropdown.Toggle variant="secondary" id="dropdown-basic">
+                        {selectedRegion ? selectedRegion.name : '選擇地區'}
+                    </Dropdown.Toggle>
+
+                    <Dropdown.Menu>
+
+                        {regions.map((region, index) => (
+                            <Dropdown.Item href="#/action-1" onClick={() => handleRegionSelect(region)} 
+                                active={selectedRegion && region.name === selectedRegion.name }
+                            >{region.name}</Dropdown.Item>
+                        ))}
+                    </Dropdown.Menu>
+                </Dropdown>
+            </div>
+
         </>
     );
 };
